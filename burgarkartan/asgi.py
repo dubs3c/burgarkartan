b1cfba8fe81d226f0e85840401e8c0eb09077518
dubs3c <dubs3c@users.noreply.github.com>
2023-08-12 23:14:47 +0200

    change project name

--- burgarkartan/asgi.py
+++ burgarkartan/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hamburgarekartan.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'burgarkartan.settings')
 
 application = get_asgi_application()
--- burgarkartan/urls.py
+++ burgarkartan/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for hamburgarekartan project.
+URL configuration for burgarkartan project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
